8c2b93d5a4e8198a926de1b810723db4e78bd162
ahsan-62 <ahsan.cse.101792@gmail.com>
2023-02-21 16:22:20 +0600

    admin authentication

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\backend\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +17,31 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
+
+// Route::group(['prefix' => 'admin'], function () {
+
+//     Route::get('/home', [HomeController::class, 'index'])->name('home');
+// });
+
+
+Route::prefix('admin/')->group(function(){
+
+Route::get('/login',[LoginController::class,'loginpage'])->name('admin.loginpage');
+Route::post('/login',[LoginController::class,'login'])->name('admin.login');
+Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
+});
+
+});
+
+Route::get('/admin', function () {
     return view('backend.pages.dashboard');
+});
+
+Route::get('/', function () {
+    return view('frontend.pages.home');
 });
 
 Auth::routes();
