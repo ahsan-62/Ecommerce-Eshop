6b47a7bdcd39cb529ccf2a71e87c08972e89d91c
ahsan-62 <ahsan.cse.101792@gmail.com>
2023-02-24 01:28:01 +0600

    Testimonial CRUD Dynamic

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -5,85 +5,20 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <title>Eshop | @yield('title')</title>
+    <title>Admin Template | @yield('title')</title>
     <meta name="description" content="Ecommerce Dashboard" />
 
     @include('backend.layouts.inc.style')
+
 </head>
 
 <body>
     <div id="root">
         <div id="nav" class="nav-container d-flex">
             <div class="nav-content d-flex">
-
                 @include('backend.layouts.inc.logo')
-                @include('backend.layouts.inc.user-menu')
 
-                <!-- Icons Menu Start -->
-                <ul class="list-unstyled list-inline text-center menu-icons">
-                    <li class="list-inline-item">
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#searchPagesModal">
-                            <i data-cs-icon="search" data-cs-size="18"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#" id="pinButton" class="pin-button">
-                            <i data-cs-icon="lock-on" class="unpin" data-cs-size="18"></i>
-                            <i data-cs-icon="lock-off" class="pin" data-cs-size="18"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#" id="colorButton">
-                            <i data-cs-icon="light-on" class="light" data-cs-size="18"></i>
-                            <i data-cs-icon="light-off" class="dark" data-cs-size="18"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#" data-bs-toggle="dropdown" data-bs-target="#notifications" aria-haspopup="true"
-                            aria-expanded="false" class="notification-button">
-                            <div class="position-relative d-inline-flex">
-                                <i data-cs-icon="bell" data-cs-size="18"></i>
-                                <span class="position-absolute notification-dot rounded-xl"></span>
-                            </div>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end wide notification-dropdown scroll-out"
-                            id="notifications">
-                            <div class="scroll">
-                                <ul class="list-unstyled border-last-none">
-                                    <li class="mb-3 pb-3 border-bottom border-separator-light d-flex">
-                                        <img src="img/profile/profile-1.jpg"
-                                            class="me-3 sw-4 sh-4 rounded-xl align-self-center" alt="..." />
-                                        <div class="align-self-center">
-                                            <a href="#">Joisse Kaycee just sent a new comment!</a>
-                                        </div>
-                                    </li>
-                                    <li class="mb-3 pb-3 border-bottom border-separator-light d-flex">
-                                        <img src="img/profile/profile-2.jpg"
-                                            class="me-3 sw-4 sh-4 rounded-xl align-self-center" alt="..." />
-                                        <div class="align-self-center">
-                                            <a href="#">New order received! It is total $147,20.</a>
-                                        </div>
-                                    </li>
-                                    <li class="mb-3 pb-3 border-bottom border-separator-light d-flex">
-                                        <img src="img/profile/profile-3.jpg"
-                                            class="me-3 sw-4 sh-4 rounded-xl align-self-center" alt="..." />
-                                        <div class="align-self-center">
-                                            <a href="#">3 items just added to wish list by a user!</a>
-                                        </div>
-                                    </li>
-                                    <li class="pb-3 pb-3 border-bottom border-separator-light d-flex">
-                                        <img src="img/profile/profile-6.jpg"
-                                            class="me-3 sw-4 sh-4 rounded-xl align-self-center" alt="..." />
-                                        <div class="align-self-center">
-                                            <a href="#">Kirby Peters just sent a new message!</a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-                <!-- Icons Menu End -->
+                @include('backend.layouts.inc.user-menu')
 
                 @include('backend.layouts.inc.menu')
 
@@ -96,20 +31,22 @@
                     <!-- Menu Button End -->
                 </div>
                 <!-- Mobile Buttons End -->
+
             </div>
             <div class="nav-shadow"></div>
         </div>
 
         <main>
-            <div class="container mt-5">
+            <div class="container" style="margin-top: 50px;">
                 @yield('admin_content')
             </div>
         </main>
+
+
         @include('backend.layouts.inc.footer')
     </div>
 
     @include('backend.layouts.inc.script')
-
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\auth\LoginController;
-use App\Http\Controllers\backend\DashboardController;
 use App\Http\Controllers\backend\CategoryController;
+use App\Http\Controllers\backend\DashboardController;
+use App\Http\Controllers\backend\TestimonialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,15 +30,16 @@ Route::prefix('admin/')->group(function(){
 
 Route::get('/login',[LoginController::class,'loginpage'])->name('admin.loginpage');
 Route::post('/login',[LoginController::class,'login'])->name('admin.login');
-Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
+
 
 Route::middleware(['auth'])->group(function(){
+    Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
     Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
     Route::resource('category', CategoryController::class);
+    Route::resource('testimonial',TestimonialController::class);
+
+
 });
-
-
-
 });
 
 
@@ -45,7 +47,5 @@ Route::middleware(['auth'])->group(function(){
 Route::get('/', function () {
     return view('frontend.pages.home');
 });
-
-Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
